fa24a15104e503647ae13fd9a70944f6c33dbec3
Sergey Kucherenko <kucherenko.web@gmail.com>
2019-05-19 22:14:53 +0300

    Delete index.js

--- src/configs/index.js
+++
@@ -1,7 +0,0 @@
-
-// Firestore
-export const apiKey = "AIzaSyCSfZ0NCBnH996mbhsWEbBbUzqTDSBsjvA";
-export const projectId = 'mib-blog-59dd5';
-export const authDomain = `${projectId}.firebaseapp.com`;
-export const databaseURL = `https://${projectId}.firebaseio.com`;
-export const storageBucket = `${projectId}.appspot.com`;
